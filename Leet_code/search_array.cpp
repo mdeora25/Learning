6e6dafa66841376e19448b74a3520612e0624e28
Mahip Deora <mahip.deora@gmail.com>
2019-08-20 14:15:56 -0500

    added the daily leet code question

---
+++ Leet_code/search_array.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for (int i =0; i < nums.size();i++) {
+            if (nums[i] == target) return true;
+        }
+        return false;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        bool ret = Solution().search(nums, target);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
