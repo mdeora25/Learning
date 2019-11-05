f6f3a23431482328fbd82ca056f0546f2b1f0304
Mahip Deora <mahip.deora@gmail.com>
2019-11-05 12:28:00 -0600

    potd

---
+++ prob_of_day/potd-q46/Heap.cpp
@@ -0,0 +1,48 @@
+#include "Heap.h"
+#include <iostream>
+
+void Heap::_percolateDown(int hole) {
+    // your code here
+    int left = hole*2;
+    int right = hole*2+1;
+    int smallest = hole;
+    if (left<size() && _data[left]>_data[smallest]) smallest=left;
+    if (right<size() && _data[right]>_data[smallest]) smallest=right;
+    if (smallest!=hole) {
+        swap(_data[hole], _data[smallest]);
+        _percolateDown(smallest);
+    }
+}
+
+int Heap::size() const {
+    return _data.size();
+}
+
+void Heap::enQueue(const int &x){
+    _data.push_back(x);
+    int hole = _data.size() - 1;
+    for(; hole > 1 && x > _data[hole/2]; hole /= 2){
+        _data[hole] = _data[hole/2];
+    }
+    _data[hole] = x;
+}
+
+int Heap::deQueue(){
+    int minItem = _data[1];
+    _data[1] = _data[_data.size() - 1];
+    _data.pop_back();
+    _percolateDown(1);
+    return minItem;
+}
+
+void Heap::printQueue(){
+    std::cout << "Current Priority Queue is: ";
+    for(auto i = _data.begin() + 1; i != _data.end(); ++i){
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> & Heap::getData() {
+    return _data;
+}
